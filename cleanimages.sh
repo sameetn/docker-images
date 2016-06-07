bfe4d03b67bab88b0ec5854066869298e81926a9
Sameet Nasnodkar <sameet.nasnodkar@gmail.com>
2016-06-06 23:29:45 -0400

    Corrected the grep command to ignore the headers

--- cleanimages.sh
+++ cleanimages.sh
@@ -1,5 +1,5 @@
 echo "Cleaning ghost docker containers..."
-docker ps -a | grep Exited | tr -s " " | cut -d " " -f1 | xargs docker rm
+docker ps -a | grep -v "CONTAINER ID" | grep Exited | tr -s " " | cut -d " " -f1 | xargs docker rm
 
 echo "Cleaning docker images with no tags"
-docker images | grep "$1"  | tr -s " " " " | cut -d " " -f3 | xargs docker rmi
+docker images | grep-v "REPOSITORY" | grep "$1"  | tr -s " " " " | cut -d " " -f3 | xargs docker rmi
